bf75f164b9ca5033c128f3505ded00ddab47b452
Gabriel Cruz <gabriel.cruz@ecomp.ufsm.br>
2024-11-22 12:28:52 -0300

    ready to begin

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Ola Clinica Medica!!!!"
+    return "fazer em quantidade não é fácil"
 
 if __name__ == '__main__':
     app.run(debug=True)
